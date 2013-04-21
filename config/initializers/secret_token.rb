d04ddd015fe211ba85b7369d6ebcabc1cec97892
pathfinder <chm2920@gmail.com>
2013-04-21 23:45:32 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = 'c2cd9d2cfd88e1ffe3d17430615977512c4bf7b1c6853f7c69809c0064bb2c808fdc0e08a4dadeb934e15324800bb8d2eab4d4661c886d48cc821cba4e9c0850'
---
+++ config/routes.rb
@@ -0,0 +1,40 @@
+Cms::Application.routes.draw do
+  
+  match 'about/(:action)' => 'about#:action'
+  
+  match 'itemcats' => 'api#itemcats'
+  match 'ct/(:id)' => 'api#ct'
+  match 'i/(:id)' => 'api#i'
+  
+  match 'q' => 'api#q'
+  match 'q/(:id)' => 'api#q'
+  
+  match 'phone' => 'start#phone'  
+  
+  match "admin" => "account#login"
+  get "account/main"
+  get "account/desktop"
+  match "admin_login_rst" => "account#login_rst"
+  match "admin_logout" => "account#logout"
+  
+  namespace :admin do  
+    post "mo_brands/index"
+    resources :mo_brands
+      
+    post "run_logs/index"
+    get "run_logs/clear"
+    resources :run_logs
+    
+    resources :admins
+  end
+  
+  namespace :kindeditor do
+    post "/upload" => "assets#create"
+    get  "/filemanager" => "assets#list"
+  end
+
+  root :to => "start#index"
+  
+  match "*path" => "application#render_not_found"
+  
+end
